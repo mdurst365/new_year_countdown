e560c02f79080736ff1eba6c2dc470cf87853f29
mdurst365 <71846329+mdurst365@users.noreply.github.com>
2020-12-20 16:33:21 -0500

    testing countdown complete message

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,11 @@ const container = document.getElementById('container');
 
 const currentYear = new Date().getFullYear();
 
+//testing New Year Message
+const timeNextYear = new Date(`January 01 2020 00:00:00`);
+/*
 const timeNextYear = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+*/
 
 function updateContainer (){
     const timeNow = new Date();
@@ -28,9 +32,15 @@ function updateContainer (){
     minutes.innerHTML = m +(" minutes");
     hours.innerHTML = h +(" hours");;
     days.innerHTML = d + (" days");
+
+    //New Year's Message
+    if (result <0) {
+        document.getElementById("title").innerHTML = "Happy New Year!"
+    }
 }
 
 setInterval(updateContainer, 1000);
+
 
 
 
